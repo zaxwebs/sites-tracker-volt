a81eb4c5c42f29df9e15f65e7184f75f353d8425
zaxwebs <zifidigital@gmail.com>
2024-10-18 19:19:01 +0530

    added livewire counter example

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Livewire\Counter;
 
 Route::get('/', function () {
-    return view('welcome');
+	return view('welcome');
 });
+
+Route::get('/counter', Counter::class);
