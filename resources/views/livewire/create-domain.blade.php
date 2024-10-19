3be55c62eca0735ed3856fbb20d1cb3c4f8caad7
zaxwebs <zifidigital@gmail.com>
2024-10-19 08:52:38 +0530

    Add tags functionality to create domain form

--- resources/views/livewire/create-domain.blade.php
+++ resources/views/livewire/create-domain.blade.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Tag;
 use App\Models\Domain;
 use Livewire\Volt\Component;
 use Livewire\Attributes\Validate;
@@ -11,6 +12,15 @@ new class extends Component {
 
 	public $description = '';
 
+	public $tags = [];
+
+	public $selectedTags = [];
+
+	public function mount()
+	{
+		$this->tags = Tag::all(); // Get all tags to populate the dropdown
+	}
+
 	public function save()
 	{
 		$this->validate();
@@ -21,11 +31,16 @@ new class extends Component {
 			'description' => $this->description
 		]);
 
-		$this->reset();
+		// Attach the selected tags to the domain
+		$domain->tags()->sync($this->selectedTags);
+
+		$this->reset(['name', 'description', 'selectedTags']);
 	}
 
-}; ?>
-<form wire:submit="save">
+};
+?>
+
+<form wire:submit.prevent="save">
 	<div class="mb-4">
 		<div>Name</div>
 		<x-text-input type="text" wire:model.blur="name" />
@@ -36,6 +51,14 @@ new class extends Component {
 	<div class="mb-4">
 		<div>Description</div>
 		<x-text-input type="text" wire:model="description" />
+	</div>
+	<div class="mb-4">
+		<div>Tags</div>
+		<select wire:model="selectedTags" multiple class="w-full max-w-48">
+			@foreach($tags as $tag)
+				<option value="{{ $tag->id }}">{{ $tag->name }}</option>
+			@endforeach
+		</select>
 	</div>
 	<div>
 		<x-primary-button>
