d174ac0d38429fc09c41c949fed86d5005cedd18
zaxwebs <zifidigital@gmail.com>
2024-10-20 07:41:46 +0530

    added wirespy with custom binds

---
+++ config/wire-spy.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+	/*
+	 * Enable or disable WireSpy.
+	 * By default, WireSpy will only be enabled in your development environment.
+	 */
+	'enabled' => env('WIRE_SPY_ENABLED'),
+
+	/**
+	 * The keybinding configuration option allows you to define a keyboard shortcut
+	 * using AlpineJS syntax. It accepts a string representing the desired key combination.
+	 *
+	 * Syntax:
+	 * - 'super' corresponds to the 'Cmd' key on macOS and the 'Ctrl' key on Windows/Linux.
+	 * - Combine with other keys using dot notation, like 'super.l' for 'Cmd+L' or 'Ctrl+L'.
+	 */
+	'keybinding' => 'shift.v',
+];
